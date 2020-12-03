fa91be9470974aa9ef60fcdb3405d05fd5d0cd1e
Matthew Betts <bettsmatthew@googlemail.com>
2020-12-03 09:22:36 +0100

    turned off Catalyst debugging messages for production server

--- lib/Fist.pm
+++ lib/Fist.pm
@@ -17,7 +17,6 @@ use Catalyst::Runtime 5.80;
 #                 directory
 
 use Catalyst qw/
-    -Debug
     ConfigLoader
     Static::Simple
 /;
@@ -46,7 +45,8 @@ __PACKAGE__->config(
 );
 
 # Start the application
-__PACKAGE__->setup(qw(-Debug));
+#__PACKAGE__->setup(qw(-Debug));
+__PACKAGE__->setup();
 
 
 =head1 NAME
